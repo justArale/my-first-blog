594b0a7900bc184885848f59e343b4e724aca199
justArale <just1arale@gmail.com>
2024-04-21 14:28:48 +0200

    correct file-path

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lc@yo20&gzp3&v&_0spv5funp5kk0zz27ae*ffy!x!&1&_$06%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.arale.uber.space']
+ALLOWED_HOSTS = ['127.0.0.1', 'arale.uber.space/my-first-blog/']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'html/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
